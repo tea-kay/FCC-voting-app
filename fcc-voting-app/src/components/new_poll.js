274a88734dc022934af7dd7a4dc3ba12baa7a86d
Timothy Kim <tkim1202@gmail.com>
2018-01-09 20:12:53 -0800

    Added textarea and button for NewPoll

--- fcc-voting-app/src/components/new_poll.js
+++ fcc-voting-app/src/components/new_poll.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Row, Jumbotron, FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
 import NavBar from './nav_bar';
 
 export default class NewPoll extends Component {
@@ -8,7 +8,30 @@ export default class NewPoll extends Component {
       <div>
         <NavBar />
         <div className="container">
-          <h1>Create a new poll.</h1>
+          <Row>
+          <Jumbotron className="text-left">
+            <h2>Create a new poll.</h2>
+            <form>
+              <FormGroup
+                controlId="formBasicText"
+              >
+                <ControlLabel>Title of New Poll:</ControlLabel>
+                <FormControl
+                  type="text"
+                  // value={this.state.value}
+                  placeholder="Enter text"
+                  // onChange={this.handleChange}
+                />
+                <FormControl.Feedback />
+                <ControlLabel>Add Options Separated by Commas:</ControlLabel>
+                <FormControl
+                  type="textarea"
+                />
+                <Button>Create Poll</Button>
+              </FormGroup>
+            </form>
+          </Jumbotron>
+        </Row>
         </div>
       </div>
     )
