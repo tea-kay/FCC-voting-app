cc81eafafb07aab26aa2e871eded3a4002cf72fb
Timothy Kim <tkim1202@gmail.com>
2018-01-30 20:41:05 -0800

    Built footer component

---
+++ fcc-voting-app/src/components/footer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Footer extends Component {
+  render() {
+    return (<footer className='footer'><div className="container"><span>Built by <a href="">@teakay</a>.</span></div></footer>);
+  }
+}
--- fcc-voting-app/src/components/landing_page.js
+++ fcc-voting-app/src/components/landing_page.js
@@ -3,6 +3,7 @@ import { Jumbotron, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import NavBar from './nav_bar';
+import Footer from './footer';
 
 export default class LandingPage extends Component {
   render() {
@@ -11,16 +12,26 @@ export default class LandingPage extends Component {
         <NavBar history={this.props.history}/>
         <div className="container">
           <Jumbotron className="jumbotron">
-            <h2>Welcome To Free-Voting-App</h2>
+            <h2>Welcome To FCC-Voting-App</h2>
             <p>Create custom polls with live results</p>
             <Button bsStyle="primary" componentClass={Link} to='/polls' href='/polls'>View Polls</Button>
           </Jumbotron>
           <Row className="show-grid">
-            <Col xs={6} md={4} className="landing-content"><span><h3>Live Vote Tracking</h3></span><p>content</p></Col>
-            <Col xs={6} md={4} className="landing-content"><span><h3>Works Anywhere</h3></span><p>content</p></Col>
-            <Col xsHidden md={4} className="landing-content"><span><h3>Integrate Social</h3></span><p>content</p></Col>
+            <Col xs={6} md={4} className="landing-content">
+              <span><h3>Live Vote Tracking</h3>
+            </span><p>Every vote is updated live and then immediately applied.</p>
+            </Col>
+            <Col xs={6} md={4} className="landing-content">
+              <span><h3>Works Anywhere</h3>
+            </span><p>You can vote with or without an account from whereever you are.</p>
+            </Col>
+            <Col xsHidden md={4} className="landing-content">
+              <span><h3>Poll Are Customizable</h3>
+            </span><p>Feel free to add additional options to polls with an account.</p>
+            </Col>
           </Row>
         </div>
+        <Footer />
       </div>
     );
   }
